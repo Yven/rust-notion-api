1de2c26ac8e8aabc442a380ed864e256e1141636
Yven <yvenchang@163.com>
2023-02-20 18:03:08 +0800

    feat(mod,request)抽象notion结构和请求结构

--- src/notion/mod.rs
+++ src/notion/mod.rs
@@ -6,122 +6,67 @@ pub mod sort;
 pub mod filter;
 pub mod property;
 pub mod block;
+pub mod request;
 
 
 use super::CONFIG_MAP;
+use filter::Filter;
+use sort::Sort;
 
 use error::CommErr;
-use term::ReqBody;
-
-use reqwest::{self, header::{HeaderMap, HeaderValue, CONTENT_TYPE}};
-use std::time::Duration;
 use serde_json::Value;
 
 
-const NOTION_URL: &str = "https://api.notion.com/v1/";
-const REQ_TIME_S: u64 = 10;
-const REQ_TIME_NS: u32 = 0;
-
-
 #[allow(dead_code)]
-#[derive(Default)]
-pub enum NotionModule {
-    #[default] Databases,
-    Pages,
-    Blocks,
-    Users,
+pub enum Module {
+    Databases(String),
+    Pages(String),
+    Blocks(String),
+    Users(String),
 }
 
-impl NotionModule {
-    fn path(&self, id: &str) -> String {
+impl Module {
+    pub fn path(&self) -> String {
         match self {
-            NotionModule::Databases => {
-                "databases/".to_string() + id + "/query"
-            },
-            NotionModule::Pages => {
-                "pages/".to_string() + id
-            },
-            NotionModule::Blocks => {
-                "blocks/".to_string() + id + "/children"
-            },
-            NotionModule::Users => {
-                "users/".to_string() + id
-            },
+            Module::Databases(id) => "databases/".to_string() + &id + "/query",
+            Module::Pages(id) => "pages/".to_string() + &id,
+            Module::Blocks(id) => "blocks/".to_string() + &id + "/children",
+            Module::Users(id) => "users/".to_string() + &id,
         }
     }
 }
 
 
-#[allow(dead_code)]
-#[derive(Default)]
-pub struct Request {
-    secret_key: String,
+pub struct Notion {
+    pub module: Module,
+    filter: Filter,
+    sort: Sort,
 }
 
-impl Request {
-    pub fn new() -> Self {
-        Request {
-            secret_key: CONFIG_MAP.get("key").unwrap().to_string(),
-        }
+impl Notion {
+    fn new(module: Module) -> Self {
+        Notion { module, filter: Filter::default(), sort::default() }
     }
 
-    fn get_header(&self) -> HeaderMap {
-        let mut header = HeaderMap::new();
-        header.insert("Notion-Version", "2022-06-28".parse().unwrap());
-        header.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-        header
+    pub fn filter(self, condition: Filter) -> Self {
+        self.filter = condition;
+        self
     }
 
-    pub fn query(&self, module: NotionModule, id: &str, body: ReqBody) -> Result<Value, CommErr> {
-        if let NotionModule::Databases = module {
-        } else {
-            return Err(CommErr::CErr("unsupport module for this method".to_string()));
-        }
-
-        let body = serde_json::from_str::<serde_json::Value>(&body.to_string()).unwrap();
-
-        let client = reqwest::blocking::Client::new();
-        let res = client.post(NOTION_URL.to_string() + &module.path(id))
-            .bearer_auth(&self.secret_key)
-            .headers(self.get_header())
-            .timeout(Duration::new(REQ_TIME_S, REQ_TIME_NS))
-            .json(&body)
-            .send()?;
-
-        let is_success = res.status().is_success();
-        let res: Value = serde_json::from_str(res.text()?.as_str())?;
-        if is_success {
-            Ok(res)
-        } else {
-            Err(CommErr::CErr(get_value_str(&res, "message")))
-        }
+    pub fn sort(self, order: Sort) -> Self {
+        self.sort = order;
+        self
     }
 
-    fn get(&self, module: NotionModule, id: &str) -> Result<Value, CommErr> {
-        let client = reqwest::blocking::Client::new();
-        let res = client.get(NOTION_URL.to_string() + &module.path(id))
-            .bearer_auth(&self.secret_key)
-            .headers(self.get_header())
-            .timeout(Duration::new(REQ_TIME_S, REQ_TIME_NS))
-            .send()?;
-
-        let is_success = res.status().is_success();
-        let res: Value = serde_json::from_str(res.text()?.as_str())?;
-        if is_success {
-            Ok(res)
-        } else {
-            Err(CommErr::CErr(get_value_str(&res, "message")))
-        }
-    }
-
-    // fn save(&self, module: NotionModule) {
+    // pub fn search(&self) -> T {
     // }
 
-    // fn update(&self, module: NotionModule) {
+    // pub fn find(&self) -> T {
     // }
 
-    // fn delete(&self, module: NotionModule) {
-    // }
+    fn format_body(&self) -> Value {
+        serde_json::from_str::<serde_json::Value>(&format!(r#"{{"filter": {},"sorts": {}}}"#, self.filter, self.sort)).unwrap()
+    }
 }
 
 
